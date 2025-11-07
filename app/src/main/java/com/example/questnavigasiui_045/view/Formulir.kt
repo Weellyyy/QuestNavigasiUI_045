16ac88a57db4db4846af98ba89d46f285c371978
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 15:20:56 +0700

    membuat radio button

--- app/src/main/java/com/example/questnavigasiui_045/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_045/view/Formulir.kt
@@ -2,12 +2,14 @@ package com.example.questnavigasiui_045.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.questnavigasiui_045.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,6 +52,18 @@ fun FormIsian(
             HorizontalDivider(modifier = Modifier
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness, color = Color.Red)
+            Row {
+                jenisK.forEach {
+                    item->
+                    Row (verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
         }
 
     }
