97df53a81b58f1677c074ab253d8571126f2b026
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 18:33:24 +0700

    menambah import

--- app/src/main/java/com/example/questnavigasiui_045/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_045/Navigasi.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigasiui_045
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
