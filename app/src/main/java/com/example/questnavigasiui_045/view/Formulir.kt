15f98c69dc70f7c97dc320dbc04dae3616a44b76
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 15:00:42 +0700

    membuat function bernama FormIsian

--- app/src/main/java/com/example/questnavigasiui_045/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_045/view/Formulir.kt
@@ -1,2 +1,11 @@
 package com.example.questnavigasiui_045.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){}
