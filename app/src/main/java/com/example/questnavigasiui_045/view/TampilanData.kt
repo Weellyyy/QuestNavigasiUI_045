b807b92adf4cc1672962a84f3a68726f3431ed03
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 16:20:17 +0700

    membuat topapp bar

--- app/src/main/java/com/example/questnavigasiui_045/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_045/view/TampilanData.kt
@@ -1,10 +1,29 @@
 package com.example.questnavigasiui_045.view
 
+import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigasiui_045.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,5 +36,11 @@ fun TampilData(
         Pair(stringResource(R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(R.string.alamat), "Yogyakarta"),
     )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
 
 }
