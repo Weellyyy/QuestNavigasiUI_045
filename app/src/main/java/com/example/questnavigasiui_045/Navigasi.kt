8e8b5f7806819158f43a0b8c5d62310b50957626
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 18:31:19 +0700

    menambah navigasi dari Formulirku ke beranda saat tombol submit diklik

--- app/src/main/java/com/example/questnavigasiui_045/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_045/Navigasi.kt
@@ -11,5 +11,27 @@ enum class Navigasi{
 @Composable
 fun DataApp(
     navController: NavHostController = rememberNavController()
-) {
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive=false)
 }
