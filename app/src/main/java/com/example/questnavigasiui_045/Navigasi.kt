9539c65e9f58c37bd5d70beaf90d9587c3b460a7
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 18:28:11 +0700

    membuat function

--- app/src/main/java/com/example/questnavigasiui_045/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_045/Navigasi.kt
@@ -1,6 +1,15 @@
 package com.example.questnavigasiui_045
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi{
     Formulirku,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
 }
