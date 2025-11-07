9d6a98988e65a26fbdb753db27d9705a1329e9e3
Weellyyy <wildanprtm2304@gmail.com>
2025-11-07 16:01:46 +0700

    membuat function bernama TampilData

--- app/src/main/java/com/example/questnavigasiui_045/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_045/view/TampilanData.kt
@@ -1,2 +1,14 @@
 package com.example.questnavigasiui_045.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_045.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () -> Unit,
+)
